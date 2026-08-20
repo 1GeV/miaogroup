ea3429f833dc95e43487352750798b92898a4904
胡哲文 <huzhw23@mail2.sysu.edu.cn>
2026-08-21 00:54:27 +0800

    Fix GitHub Pages base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://1gev.github.io',
-  base: '/miaogroup',
+  base: '/miaogroup/',
   integrations: [sitemap()],
   markdown: { shikiConfig: { theme: 'github-light' } }
 });
